9d85c24606e817bc08c177dcdab36cee7d45f074
RickK213 <rkippert@att.net>
2018-01-24 11:55:19 -0600

    Added KeyManager

---
+++ BandMate/KeyManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BandMate
+{
+    public class KeyManager
+    {
+        //member variables
+
+        //properties
+        public string SendGridKey { get; set; }
+        //constructor
+        public KeyManager()
+        {
+            SetKeys();
+        }
+
+        //member methods
+        void SetKeys()
+        {
+
+        }
+    }
+}
